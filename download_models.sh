274a6e0dee248275da2b65358ef01d7b0803fcf6
Débora Laís <123045451+deboralais@users.noreply.github.com>
2025-06-10 15:31:10 -0300

    Add newline and correct paths

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
+mkdir -p app/model
 
-mkdir -p backend/app/model
+gdown --id 1y3z79VfkMRs-frZIoy4R-dLLYhiCF3iS -O app/model/modelo_bert.pth
 
-gdown --id 1y3z79VfkMRs-frZIoy4R-dLLYhiCF3iS -O backend/app/model/modelo_bert.pth
-
-gdown --id 1cy8BNQcBM_thnPnhT7-7lljQHbMkil0s -O backend/app/model/modelo_gpt2.pth
+gdown --id 1cy8BNQcBM_thnPnhT7-7lljQHbMkil0s -O app/model/modelo_gpt2.pth
